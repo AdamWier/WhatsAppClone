307958ee43ad143b1f303e386964cfad7184db8d
AdamWier <38018787+AdamWier@users.noreply.github.com>
2019-10-06 13:04:32 +0200

    Changed list to display not all contacts but only contacts with an account. Finished at 1:39

--- app/src/main/java/com/adamwier/whatsappclone/FindUserActivity.java
+++ app/src/main/java/com/adamwier/whatsappclone/FindUserActivity.java
@@ -1,5 +1,6 @@
 package com.adamwier.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,7 +8,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.telephony.TelephonyManager;
 import android.widget.LinearLayout;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -16,13 +25,14 @@ public class FindUserActivity extends AppCompatActivity {
     private RecyclerView mUserList;
     private RecyclerView.Adapter mUserListAdapter;
     private RecyclerView.LayoutManager mUserListLayourManager;
-    ArrayList<UserObject> userList;
+    ArrayList<UserObject> contactList, userList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_user);
 
+        contactList = new ArrayList<>();
         userList = new ArrayList<>();
 
         initalizeRecycleView();
@@ -30,16 +40,75 @@ public class FindUserActivity extends AppCompatActivity {
     }
 
     private void getContactList(){
+        String ISOPRefix = getCountryISO();
+
         Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
         while (phones.moveToNext()){
             String name = phones.getString(phones.getColumnIndex((ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)));
             String phone = phones.getString(phones.getColumnIndex((ContactsContract.CommonDataKinds.Phone.NUMBER)));
 
-            UserObject mContacts = new UserObject(name, phone);
-            userList.add(mContacts);
+            phone = phone.replace(" ", "");
+            phone = phone.replace("-", "");
+            phone = phone.replace("(", "");
+            phone = phone.replace(")", "");
+
+            if (!String.valueOf(phone.charAt(0)).equals("+")){
+                phone = ISOPRefix + phone;
+            }
+
+            UserObject mContact = new UserObject(name, phone);
+            contactList.add(mContact);
             mUserListAdapter.notifyDataSetChanged();;
+System.out.println(mContact);
+            getUserDetails(mContact);
         }
     }
+
+    private void getUserDetails(UserObject mContact){
+        DatabaseReference mUserDB = FirebaseDatabase.getInstance().getReference().child("user");
+        Query query = mUserDB.orderByChild("phone").equalTo(mContact.getPhone());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    String phone = "";
+                    String name = "";
+                    for(DataSnapshot childsnapshot : dataSnapshot.getChildren()){
+                       if (childsnapshot.child("phone").getValue() != null){
+                           phone = childsnapshot.child("phone").getValue().toString();
+                       }
+                        if (childsnapshot.child("name").getValue() != null){
+                            name = childsnapshot.child("name").getValue().toString();
+                        }
+
+                        UserObject mUser = new UserObject(name, phone);
+                        userList.add(mUser);
+                        mUserListAdapter.notifyDataSetChanged();
+                        return;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private String getCountryISO(){
+        String iso = null;
+
+        TelephonyManager telephonyManager = (TelephonyManager) getApplicationContext().getSystemService(getApplicationContext().TELEPHONY_SERVICE);
+        if (telephonyManager.getNetworkCountryIso() != null){
+            if (!telephonyManager.getNetworkCountryIso().toString().equals("")){
+                iso = telephonyManager.getNetworkCountryIso().toString();
+            }
+        }
+
+        return CountryToPhonePrefix.getPhone(iso);
+    }
+
     private void initalizeRecycleView() {
         mUserList = findViewById(R.id.userList);
         mUserList.setNestedScrollingEnabled(false);
